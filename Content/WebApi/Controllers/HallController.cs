2a30f3e4b59b77d9d055439522c7eed6d1897874
Legio <legiosys@gmail.com>
2020-03-09 17:19:24 +0300

    Added Hall Controller

---
+++ Content/WebApi/Controllers/HallController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HallController : ControllerBase
+    {
+        private readonly DomainContext _context;
+
+        public HallController(DomainContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Halls
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hall>>> GetHalls()
+        {
+            return await _context.Halls.ToListAsync();
+        }
+
+        // GET: api/Halls/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hall>> GetHall(int id)
+        {
+            var hall = await _context.Halls.FindAsync(id);
+
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return hall;
+        }
+
+        // PUT: api/Halls/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHall(int id, Hall hall)
+        {
+            if (id != hall.HallId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hall).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HallExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Halls
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Hall>> PostHall(Hall hall)
+        {
+            _context.Halls.Add(hall);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHall", new { id = hall.HallId }, hall);
+        }
+
+        // DELETE: api/Halls/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Hall>> DeleteHall(int id)
+        {
+            var hall = await _context.Halls.FindAsync(id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            _context.Halls.Remove(hall);
+            await _context.SaveChangesAsync();
+
+            return hall;
+        }
+
+        private bool HallExists(int id)
+        {
+            return _context.Halls.Any(e => e.HallId == id);
+        }
+    }
+}
