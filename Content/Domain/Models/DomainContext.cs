a6b8c8b1b9208623f731dc5a6b665a284f8b1f80
Legio <legiosys@gmail.com>
2020-03-09 16:00:38 +0300

    Model Hall, Row Added

---
+++ Content/Domain/Models/DomainContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Models
+{
+    public class DomainContext : DbContext
+    {
+        public DbSet<Hall> Halls { get; set; }
+        public DbSet<Row> Rows { get; set; }
+        public DomainContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=domaindb;Trusted_Connection=True;");
+        }
+    }
+}
