88e3b8916169c1f4e4973be04607e1df80812239
Alex Legio <legiosys@gmail.com>
2020-04-17 13:49:05 +0300

    Added FilmLogic classes for low coupled architecture, added connection keys to configuration file for omdb api

---
+++ Content/Domain/FilmLogic/FilmAdapter.cs
@@ -0,0 +1,80 @@
+﻿using Domain.Exceptions;
+using Domain.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.FilmLogic
+{
+    public class FilmAdapter
+    {
+        private readonly DomainContext _context;
+        private readonly ILogger _logger;
+        private readonly Validator _validator;
+        public FilmAdapter(DomainContext context, ILogger<FilmAdapter> logger, Validator validator)
+        {
+            _context = context;
+            _logger = logger;
+            _validator = validator;
+        }
+
+        private async Task<Film> GetFilmById(string id)
+        {
+            var film = await _context.Films.FirstOrDefaultAsync(f => f.ImdbId == id);
+            if (film == null) throw new NotFoundException("Film doesn't exist!", $"imdbID:{id}", "GetFilmById");
+            return film;
+        }
+        //Gets film by IMDB ID
+        public async Task<Film> GetFilm(string imdbid)
+        {
+            var film = await GetFilmById(imdbid);
+            return film;
+        }
+        //Gets all films
+        public async Task<IEnumerable<Film>> GetFilms()
+        {
+            return await _context.Films.ToListAsync();
+        }
+        //Adds new film to DB
+        public async Task AddFilm(Film postfilm)
+        {
+            var film = await _validator.Validate(postfilm, "POST");
+            _context.Films.Add(film);
+            await _context.SaveChangesAsync();
+        }
+        //Updates film
+        public async Task UpdateFilm(Film putfilm)
+        {
+            var film = await GetFilmById(putfilm.ImdbId);
+            film.Name = putfilm.Name;
+            film.Description = putfilm.Description;
+            film.StartDate = putfilm.StartDate;
+            film.FinishDate = putfilm.FinishDate;
+            film = await _validator.Validate(film, "PUT");
+            _context.Films.Update(film);
+            await _context.SaveChangesAsync();
+        }
+        //Patches film
+        public async Task PatchFilm(string imdbid, JsonPatchDocument<Film> patchFilm)
+        {
+            var film = await GetFilmById(imdbid);
+            patchFilm.ApplyTo(film);
+            film = await _validator.Validate(film, "PATCH");
+            _context.Films.Update(film);
+            await _context.SaveChangesAsync();
+        }
+        //Removes film from DB
+        public async Task<Film> DeleteFilm(string imdbid)
+        {
+            _logger.LogInformation("DELETE: {0}", imdbid);
+            var film = await GetFilmById(imdbid);
+            _context.Films.Remove(film);
+            await _context.SaveChangesAsync();
+            return film;
+        }
+    }
+}
--- Content/WebApi/Controllers/FilmController.cs
+++ Content/WebApi/Controllers/FilmController.cs
@@ -5,13 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Flurl;
-using Flurl.Http;
-using Domain.DTOs;
 using Domain.Models;
-using Domain.Exceptions;
+using Domain.FilmLogic;
 
 namespace WebApi.Controllers
 {
@@ -20,14 +15,11 @@ namespace WebApi.Controllers
     [ApiController]
     public class FilmController : ControllerBase
     {
-        private const string APIKEY = "89ca9b02";
-        private readonly DomainContext _context;
-        private readonly ILogger _logger;
+        private FilmAdapter _films;
 
-        public FilmController(DomainContext context, ILogger<HallController> logger)
+        public FilmController(FilmAdapter films)
         {
-            _context = context;
-            _logger = logger;
+            _films = films;
         }
 
         /// <summary>Shows all films</summary>
@@ -37,9 +29,9 @@ namespace WebApi.Controllers
         /// 
         /// </remarks>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
+        public async Task<IEnumerable<Film>> GetFilms()
         {
-            return await _context.Films.ToListAsync();
+            return await _films.GetFilms();
         }
 
         /// <summary>Shows film</summary>
@@ -50,9 +42,8 @@ namespace WebApi.Controllers
         /// </remarks>
         [HttpGet("{imdbid}")]
         public async Task<ActionResult<Film>> GetFilm(string imdbid)
-        {
-            var film = await GetFilmById(imdbid);          
-            return film;
+        {                
+            return await _films.GetFilm(imdbid);
         }
 
         /// <summary>Creates new film</summary>
@@ -68,11 +59,9 @@ namespace WebApi.Controllers
         ///     
         /// </remarks>
         [HttpPost]
-        public async Task<ActionResult<Film>> PostFilm(Film postfilm)
+        public async Task<IActionResult> PostFilm(Film postfilm)
         {
-            var film = await ValidateFilm(postfilm, "POST");
-            _context.Films.Add(film);
-            await _context.SaveChangesAsync();
+            await _films.AddFilm(postfilm);
             return Ok();
         }
 
@@ -91,37 +80,28 @@ namespace WebApi.Controllers
         [HttpPut]
         public async Task<IActionResult> PutFilm(Film putfilm)
         {
-            var film = await GetFilmById(putfilm.ImdbId);
-            film.Name = putfilm.Name;
-            film.Description = putfilm.Description;
-            film.StartDate = putfilm.StartDate;
-            film.FinishDate = putfilm.FinishDate;
-            film = await ValidateFilm(film, "PUT");
-            _context.Entry(film).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+            await _films.UpdateFilm(putfilm);
             return Ok();
         }
-
 
         /// <summary>Patches values</summary>
         /// <remarks>Sample request: 
         /// 
-        ///     PATCH/tt3896198 {
-        ///         "op": "replace",
-        ///         "path": "/Name",
-        ///         "value": "MegaFilm"
-        ///     }
+        ///     PATCH/tt3896198 
+        ///     [
+        ///         {
+        ///             "op": "replace",
+        ///             "path": "/Name",
+        ///             "value": "MegaFilm"
+        ///         }
+        ///     ]
         ///     
         /// </remarks>
         /// <returns></returns>
         [HttpPatch("{imdbid}")]
         public async Task<IActionResult> PatchFilm(string imdbid,JsonPatchDocument<Film> patchFilm)
         {
-            var film = await GetFilmById(imdbid);
-            patchFilm.ApplyTo(film);
-            film = await ValidateFilm(film,"PATCH");
-            _context.Entry(film).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+            await _films.PatchFilm(imdbid, patchFilm);
             return Ok();
         }
 
@@ -133,43 +113,8 @@ namespace WebApi.Controllers
         /// </remarks>
         [HttpDelete("{imdbid}")]
         public async Task<ActionResult<Film>> DeleteFilm(string imdbid)
-        {
-            _logger.LogInformation("DELETE: {0}", imdbid);
-            var film = await GetFilmById(imdbid);
-            _context.Films.Remove(film);
-            await _context.SaveChangesAsync();
-            return film;
-        }
-
-        private async Task<Film> GetFilmById(string id)
-        {
-            var film = await _context.Films.FirstOrDefaultAsync(f => f.ImdbId == id);
-            if (film == null) throw new NotFoundException("Film doesn't exist!", $"imdbID:{id}", "GetFilmById");
-            return film;
-        }
-
-        private async Task<Film> CheckFilmOnImdb(Film film)
-        {
-            var result = await "http://www.omdbapi.com"
-                .SetQueryParams(new { apikey = APIKEY, i = film.ImdbId })
-                .GetJsonAsync<Dto_ImdbFilm>();
-            _logger.LogInformation("imdb result: {0}", result.Title);
-            if (result.Title == null) throw new NotFoundException("Film doesn't exist on IMDB!", $"imdbID:{film.ImdbId}", "CheckFilmOnImdb");
-            film.Name ??= result.Title;
-            film.Description ??= result.Plot;
-            return film;
-        }
-
-        private async Task<Film> ValidateFilm (Film film, string method)
-        {
-            if (method.Equals("POST") || film.Name == null || film.Description == null)
-                film = await CheckFilmOnImdb(film);
-            _logger.LogInformation($"{method}: Changes to dbo.Films: imdbid: {film.ImdbId} \n" +
-                $"Name: {film.Name} \n Description: {film.Description} \n" +
-                $"StartDate: {film.StartDate} \n FinishDate: {film.FinishDate}");
-            if (film.StartDate > film.FinishDate) 
-                throw new BadArgumentException("Start Date should be earlier then finish!", $"Start: {film.StartDate}, Finish: {film.FinishDate}", "PostFilm");
-            return film;
-        }
+        {          
+            return await _films.DeleteFilm(imdbid);
+        }       
     }
 }
